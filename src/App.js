d052257f598b162b0d58476546765e8fdd97f215
Zelalem Gebre <zel.alem.ayehu@gmail.com>
2023-02-26 12:02:35 +0300

    First frontend commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Profile from './ProfileRegistration';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = 'container-fluid'>
+      <h1 className = 'text-center'>Simple React App</h1>
+      <div className = 'row'>
+        <div className = 'col-4'>
+          <Profile />
+        </div>
+      </div>
     </div>
-  );
+   );
 }
 
 export default App;
---
+++ src/ProfileRegistration.js
@@ -0,0 +1,55 @@
+function Profile() {
+    let [_id, setId] = useState();
+    let [name, setName] = useState();
+    let [password, setpassword] = useState();
+    let [phone, setphone] = useState();
+    let [dob, setdob] = useState();
+    let handleSubmit = (event) => {
+      event.preventDefault();
+      let URL = 'http://localhost:8181/profile';
+      axios.post(URL, {"_id": parseInt(_id) , "name": name, "password": password, "phone": phone, "dob": dob})
+      .then((response) => {
+        setId(''); setName(''); setpassword('');setphone('');setdob('');
+      })
+      .catch(err => console.log(err))
+    }
+    return (<div>
+      <h1>Profile Registration Form</h1>
+      <form onSubmit = {handleSubmit}>
+
+        <div className = 'form-group'>
+        <input type = 'number' name = 'id' value = {_id} placeholder = 'Enter id'
+        autoComplete='off' 
+        className = 'form-control' onChange = {e => setId(e.target.value)}></input>
+        </div>
+
+        <div className = 'form-group'>
+        <input type = 'tel' name = 'phone' value = {phone} 
+        autoComplete='off' 
+        className = 'form-control' onChange = {e => setId(e.target.value)}></input>
+        </div>
+        
+        <div className = 'form-group'>
+        <input type = 'password' name = 'password' value = {password} 
+        autoComplete='off' 
+        className = 'form-control' onChange = {e => setId(e.target.value)}></input>
+        </div>
+
+        <div className = 'form-group'>
+        <input type = 'tel' name = 'phone' value = {phone} 
+        autoComplete='off' 
+        className = 'form-control' onChange = {e => setId(e.target.value)}></input>
+        </div>
+
+        <div className = 'form-group'>
+        <input type = 'date' name = 'dob' value = {dob} placeholder = 'Enter name'
+        autoComplete='off'
+        className = 'form-control' onChange = {e => setName(e.target.value)}></input>
+        </div>
+        <div>
+          <input type = "submit" className = 'btn btn-primary' value = "Register"></input>
+        </div>
+      </form>
+    </div>)
+  }
+  export default Profile;
